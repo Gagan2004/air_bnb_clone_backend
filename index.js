e35c7c000f68093fd3f929f53c81ca8bda465e20
Gagan2004 <gagank90815@gmail.com>
2025-08-15 23:36:09 +0530

    CORS FIX

--- index.js
+++ index.js
@@ -20,16 +20,7 @@ app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 
 
-// app.use(cors());
-
-app.use(cors({
-  origin: [
-    "http://localhost:4000",       // local dev
-    "https://air-bnb-clone-frontend.vercel.app/" // production frontend
-  ],
-  credentials: true
-}));
-
+app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
